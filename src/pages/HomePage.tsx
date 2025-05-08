d20cf004e84702da1f292c0236f90e5ea5884019
Elias <you@example.com>
2025-05-08 18:36:49 +0200

    Created HomeButton component and added it in main

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,20 @@
-
+import HomeButton from "../components/buttons/HomeButton/HomeButton";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome to the home page!</p>
+      <HomeButton
+        title={"Utforska Borås"}
+        className={"homebutton-colored"}
+      ></HomeButton>
+      <HomeButton
+        title={"Om appen"}
+        className={"homebutton-unColored "}
+      ></HomeButton>
     </div>
   );
-}
+};
 
 export default HomePage;
