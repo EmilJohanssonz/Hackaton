ebeb33f6b58b59eef41084b91fe0c5131c3e32fa
Emil Johansson <Emilaik@gmail.com>
2025-05-09 08:47:08 +0200

    route

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import StartPage from "./pages/StartPage";
 import HomePage from "./pages/HomePage";
-import HealthcarePage from "./pages/HealthcarePage";
+import HealthcarePage from "./pages/HealthcarePage/HealthcarePage";
 import ShoppingPage from "./pages/Shopping";
 import EventPage from "./pages/Event";
 import LokaliztionPage from "./pages/LokalizationPage";
 import RootLayout from "./components/layout/rootlayout";
 import RestaurantPage from "./pages/RestaurantPage";
-import EnterprisePage from "./pages/EnterprisePage";
-import EducationPage from "./pages/educationPage";
+import EnterprisePage from "./pages/EnterprisePage/EnterprisePage";
+import EducationPage from "./pages/EducationPage/EducationPage";
 
 
 
